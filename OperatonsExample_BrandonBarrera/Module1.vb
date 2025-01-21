297aa4247617929b9d64cf5879f8eacfceddb815
Barkly-Brandon <bubba05barrera@gmail.com>
2025-01-21 08:43:47 -0700

    Added more operators

--- OperatonsExample_BrandonBarrera/Module1.vb
+++ OperatonsExample_BrandonBarrera/Module1.vb
@@ -1,4 +1,12 @@
-﻿Module Module1
+﻿'Brandon Barrera
+'Spring 2025
+'RCET2265
+'OperatorsExample
+'
+
+Option Compare Text
+
+Module Module1
 
     Sub Main()
         'sum operator
@@ -21,6 +29,30 @@
         Console.WriteLine(5 / 3) 'print the difference
         Console.WriteLine()
 
+        'Compare
+        Console.WriteLine("5>3=") 'print a string
+        Console.WriteLine(5 > 3) 'print exponent
+        Console.WriteLine()
+
+        Console.WriteLine("5<3=") 'print a string
+        Console.WriteLine(5 < 3) 'print exponent
+        Console.WriteLine()
+
+        Console.WriteLine("5=3=") 'print a string
+        Console.WriteLine(5 = 3) 'print exponent
+        Console.WriteLine()
+
+        Console.WriteLine("Is 5 not equal to 3?") 'print a string
+        Console.WriteLine(5 <> 3) 'print exponent
+        Console.WriteLine()
+
+        Console.WriteLine("A < B =") 'print a string
+        Console.WriteLine("A" < "B")
+        Console.WriteLine()
+
+        Console.WriteLine("Is " & Chr(34) & "A" & Chr(34) & " the same as " & Chr(34) & "a" & Chr(34) & " ?") 'print a string
+        Console.WriteLine("A" = "a")
+        Console.WriteLine()
 
     End Sub
 
