02dafc5911ff28b2a59f4e3bb43690ec2d683489
Barkly-Brandon <bubba05barrera@gmail.com>
2025-01-21 08:19:00 -0700

    Add project files.

---
+++ OperatonsExample_BrandonBarrera/Module1.vb
@@ -0,0 +1,27 @@
+﻿Module Module1
+
+    Sub Main()
+        'sum operator
+        Console.Write("5+3=") 'print a string
+        Console.WriteLine(5 + 3) 'print the sum
+        Console.WriteLine()
+
+        'difference operator
+        Console.Write("5-3=") 'print a string
+        Console.WriteLine(5 - 3) 'print the difference
+        Console.WriteLine()
+
+        'multiplication operator
+        Console.Write("5*3=") 'print a string
+        Console.WriteLine(5 * 3) 'print the product
+        Console.WriteLine()
+
+        'integer division operator
+        Console.Write("5/3=") 'print a string
+        Console.WriteLine(5 / 3) 'print the difference
+        Console.WriteLine()
+
+
+    End Sub
+
+End Module
---
+++ OperatonsExample_BrandonBarrera/My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("OperatonsExample_BrandonBarrera")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("OperatonsExample_BrandonBarrera")>
+<Assembly: AssemblyCopyright("Copyright ©  2025")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("37c3fa28-35df-42f2-9f37-cd3e53c74f0a")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
